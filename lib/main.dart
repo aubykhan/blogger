3fe5fd77630ecb544972ac0bf2aea9fdc8ed63b4
AUBY Khan <aubykhan@gmail.com>
2017-08-03 18:22:50 +0500

    Refine ListView

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
 import 'dart:async';
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+
+/// Steps
+/// 1. New application.
+/// 2. Change theme, title etc.
+/// 3. Add listView with refresh button (fab).
+/// 4. Use simple static data to populate the list.
+/// 5. Add http package
+/// 6. Create http client and call service to update the list.
+/// 7. Enhance UI, add cards etc.
+/// 8. Add loader.
 
 void main() {
   runApp(new MyApp());
@@ -13,35 +22,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Awesome user list',
       theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting
-        // the app, try changing the primarySwatch below to Colors.green
-        // and then invoke "hot reload" (press "r" in the console where
-        // you ran "flutter run", or press Run > Hot Reload App in
-        // IntelliJ). Notice that the counter didn't reset back to zero;
-        // the application is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
+        accentColor: Colors.red,
       ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
+      home: new MyHomePage(title: 'Awesome user list'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful,
-  // meaning that it has a State object (defined below) that contains
-  // fields that affect how it looks.
-
-  // This class is the configuration for the state. It holds the
-  // values (in this case the title) provided by the parent (in this
-  // case the App widget) and used by the build method of the State.
-  // Fields in a Widget subclass are always marked "final".
 
   final String title;
 
@@ -50,13 +42,33 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  bool _isBusy = false;
-  final List<Post> _posts = new List();
-  final List<Photo> _photos = new List();
-  final List<User> _users = new List();
+  final List<UserListTile> _listTiles = new List();
+  bool _isLoading = true;
 
   _MyHomePageState() {
     _refreshData();
+  }
+
+  Future _refreshData() async {
+    setState(() {
+      _isLoading = true;
+      _listTiles.clear();
+    });
+
+    var client = createHttpClient();
+    var response =
+        await client.get('https://jsonplaceholder.typicode.com/users');
+    List list = JSON.decode(response.body);
+
+    setState(() {
+      _isLoading = false;
+      _listTiles.addAll(list.map((item) => new UserListTile(new User(
+            item['name'],
+            item['email'],
+            item['phone'],
+            item['website'],
+          ))));
+    });
   }
 
   @override
@@ -65,37 +77,22 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: new AppBar(
         title: new Text(widget.title),
       ),
-      body: _isBusy
-          ? const Center(
-              child: const CircularProgressIndicator(),
-            )
-          : new ListView.builder(
-              itemCount: _users.length,
-              itemBuilder: (_, index) => new UserListItem(_users[index]),
+      body: _isLoading
+          ? new Center(child: const CircularProgressIndicator())
+          : new ListView(
+              children: ListTile
+                  .divideTiles(
+                    context: context,
+                    tiles: _listTiles,
+                  )
+                  .toList(),
             ),
       floatingActionButton: new FloatingActionButton(
-          child: const Icon(Icons.refresh),
-          onPressed: () {
-            _refreshData();
-          }),
+        onPressed: _refreshData,
+        tooltip: 'Refresh',
+        child: new Icon(Icons.refresh),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
-  }
-
-  Future _refreshData() async {
-    setState(() => _isBusy = true);
-
-    var client = createHttpClient();
-    var response =
-        await client.get('https://jsonplaceholder.typicode.com/users');
-    List users = JSON.decode(response.body);
-
-    setState(() {
-      _users.clear();
-      _users.addAll(
-          users.map((map) => new User(map['name'], map['email'], map['phone'], map['website'])).toList());
-
-      _isBusy = false;
-    });
   }
 }
 
@@ -108,108 +105,39 @@ class User {
   User(this.name, this.email, this.phone, this.website);
 }
 
-class UserListItem extends StatelessWidget {
+class UserListTile extends StatelessWidget {
   final User user;
 
-  UserListItem(this.user);
+  UserListTile(this.user);
 
   @override
   Widget build(BuildContext context) {
     return new Card(
       child: new Column(
-        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          new Padding(
-            padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-            child: new Text(
+          new ListTile(
+            title: new Text(
               user.name,
-              textAlign: TextAlign.left,
+              style: Theme.of(context).textTheme.display1,
               overflow: TextOverflow.ellipsis,
-              style: Theme.of(context).textTheme.display1.copyWith(color: Colors.black87),
             ),
           ),
           new ListTile(
-            leading: const Icon(Icons.email),
             title: new Text(user.email),
+            leading: const Icon(Icons.email),
           ),
           new ListTile(
-            leading: const Icon(Icons.phone),
             title: new Text(user.phone),
+            leading: const Icon(Icons.phone),
           ),
           new ListTile(
-            leading: const Icon(Icons.web),
             title: new Text(user.website),
+            leading: const Icon(Icons.web),
           ),
         ],
       ),
     );
   }
-}
-
-
-class Photo {
-  final String url;
-  final String title;
-
-  Photo(this.title, this.url);
-}
-
-class PhotoItem extends StatelessWidget {
-  final Photo photo;
-
-  PhotoItem(this.photo);
-
-  @override
-  Widget build(BuildContext context) {
-    return new Card(
-      child: new SizedBox.fromSize(
-        size: const Size.fromHeight(200.0),
-        child: new Image.network(photo.url),
-      ),
-    );
-  }
-}
-
-class PostItem extends StatelessWidget {
-  PostItem(this.post);
-
-  final Post post;
-
-  @override
-  Widget build(BuildContext context) {
-    return new InkWell(
-      onTap: () {
-        showDialog(
-          context: context,
-          child: new AlertDialog(
-            title: const Text('Item tapped'),
-            content: new Text(post.title),
-            actions: <Widget>[
-              new FlatButton(onPressed: null, child: const Text('OK'))
-            ],
-          ),
-        );
-      },
-      child: new ListTile(
-        isThreeLine: false,
-        title: new Text(
-          post.title,
-          overflow: TextOverflow.ellipsis,
-        ),
-        subtitle: new Text(
-          post.body,
-          overflow: TextOverflow.ellipsis,
-          maxLines: 2,
-        ),
-      ),
-    );
-  }
-}
-
-class Post {
-  Post(this.title, this.body);
-
-  final String title;
-  final String body;
 }
